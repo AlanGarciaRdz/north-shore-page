988dc4346530769d4263a115d4aa05a1945fd2cb
Omar Ramos Espinosa <67802294+CapiPato@users.noreply.github.com>
2022-04-27 17:48:01 -0500

    add loading on each page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,58 @@
 import 'styles/fonts.css';
 
 import { CssBaseline } from '@nextui-org/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import LoadingPage from 'src/components/base/LoadingPage';
 import ContactDataProvider from 'src/context/ContactDataContext';
 import ThemeProvider from 'src/context/ThemeContext';
 
 import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [routerState, setRouterState] = useState({
+    isRouteChanging: false,
+    loadingKey: 0,
+  });
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setRouterState((prevState) => ({
+        ...prevState,
+        isRouteChanging: true,
+        loadingKey: prevState.loadingKey ^ 1,
+      }));
+    };
+
+    const handleRouteChangeEnd = () => {
+      setRouterState((prevState) => ({
+        ...prevState,
+        isRouteChanging: false,
+      }));
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeEnd);
+    router.events.on('routeChangeError', handleRouteChangeEnd);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeEnd);
+      router.events.off('routeChangeError', handleRouteChangeEnd);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider>
       <CssBaseline />
-      <ContactDataProvider>
-        <Component {...pageProps} />
-      </ContactDataProvider>
+      <LoadingPage
+        isRouteChanging={routerState.isRouteChanging}
+        key={routerState.loadingKey}
+      >
+        <ContactDataProvider>
+          <Component {...pageProps} />
+        </ContactDataProvider>
+      </LoadingPage>
     </ThemeProvider>
   );
 }
