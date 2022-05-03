267c68ce118ffea398605c84085c72c4d7481981
Omar Ramos Espinosa <67802294+CapiPato@users.noreply.github.com>
2022-05-02 20:57:29 -0500

    fix PORT for project

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import next from 'next';
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 (async () => {
   try {
